934a011b491041469613076cc032b8f566a410fe
Basant kumar Hota <basahota@users.noreply.github.com>
2021-08-21 20:07:47 +0530

    Add files via upload

---
+++ src/main/java/com/javatechie/jpa/entity/Product.java
@@ -0,0 +1,28 @@
+package com.javatechie.jpa.entity;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "PRODUCT_TBL")
+@Data
+@NoArgsConstructor
+public class Product {
+    @Id
+    @GeneratedValue
+    private  int id;
+    private String name;
+    private int quantity;
+    private long price;
+
+    public Product(String name, int quantity, long price) {
+        this.name = name;
+        this.quantity = quantity;
+        this.price = price;
+    }
+}
